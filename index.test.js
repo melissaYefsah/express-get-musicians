34789155655259282c153bedc1f774d7be3ff842
Melissa Yefsah <melissa.yefsah@melissa.yefsah-MacBook-Pro>
2024-11-08 10:28:28 -0500

    Added Get endpoint and test

--- index.test.js
+++ index.test.js
@@ -2,21 +2,23 @@
 const { execSync } = require('child_process');
 execSync('npm install');
 execSync('npm run seed');
+const { describe, it, expect, beforeAll, afterAll } = require('@jest/globals')
 
-const request = require("supertest")
+//const request = require("supertest")
 const { db } = require('./db/connection');
 const { Musician } = require('./models/index')
 const app = require('./src/app');
 const {seedMusician} = require("./seedData");
+const request = require("supertest");
 
 
 describe('./musicians endpoint', () => {
     // Write your tests here
-    
-    
-
-
-
-
-    
+    test("Testing Music endpoint", async () => {
+        // Sends request to `/musicians` endpoint
+        const response = await request(app).get("/musicians");
+        const responseData = JSON.parse(response.text);
+        expect(response.statusCode).toBe(200);
+        expect(responseData).toMatchObject(seedMusician);
+    }) 
 })
